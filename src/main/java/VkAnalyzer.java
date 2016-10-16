ad917bb1df4281d908c526fa7403fbfc11b44f00
Sergey Verentsov <north1605@list.ru>
2016-10-16 08:38:38 +0300

    integrated with selenium

--- src/main/java/VkAnalyzer.java
+++ src/main/java/VkAnalyzer.java
@@ -1,23 +1,40 @@
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import models.UserCredentials;
+import services.Crawler;
+import services.IdService;
+import services.VkExplorerService;
+
 import java.io.PrintWriter;
 import java.util.*;
 
 public class VkAnalyzer {
-    public static List<String> generateIds(int count)
+    private static List<String> generateIds(int count)
     {
         Random r = new Random();
         LinkedList<String> ids = new LinkedList<>();
         for(int i = 0;i<count;i++) {
-            Integer next = r.nextInt(200000000);
-            ids.add(next.toString());
+            Integer next = r.nextInt(20000000);
+            ids.add("id"+next.toString());
         }
         return ids;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ClientException, ApiException {
+
+        UserCredentials userCredentials = new UserCredentials(1, "email or phone", "password");
+        IdService idService = new IdService();
+        Crawler crawler = new Crawler(userCredentials.getLogin(), userCredentials.getPassword());
+        VkExplorerService explorerService = new VkExplorerService(idService, crawler);
+        String to = "<short name of user>";
+
         HashMap<Integer,List<Integer>> sumFriendsCounts = new HashMap<>();
         HashMap<Integer,Integer> chainsLengthCountMap = new HashMap<>();//count of chains with different lengths
-        List<String> ids = generateIds(2000);
+        int idsCount = 100;
+        List<String> ids = generateIds(idsCount);
+        int c = 0;
         for(String id : ids) {
-            List<Integer> friendsCounts = getFriendChainToMe(id);
+            System.out.println(c++ +"\\"+idsCount);
+            List<Integer> friendsCounts = explorerService.getFriendsChain(id,to);
             int chainLength = friendsCounts.size();
             if(sumFriendsCounts.containsKey(chainLength)) {
                 List<Integer> currentFriendCounts = sumFriendsCounts.get(chainLength);
@@ -34,14 +51,16 @@ public class VkAnalyzer {
         }
         try {
             PrintWriter writer = new PrintWriter("output.csv", "UTF-8");
+            writer.print(";");
             for (Integer i = 0; i < 15;i++ )
                 writer.print(";"+i.toString());
             writer.println();
             for (Integer chainLength : sumFriendsCounts.keySet()) {
                 writer.print("length"+chainLength.toString()+";");
+                writer.print( chainsLengthCountMap.get(chainLength)+";");
                 for(int i = 0;i<chainLength;i++) {
                     List<Integer> sumFriendsCount = sumFriendsCounts.get(chainLength);
-                    writer.print((double) sumFriendsCount.get(i) / chainsLengthCountMap.get(chainLength) + ";");
+                    writer.print(sumFriendsCount.get(i) / chainsLengthCountMap.get(chainLength) + ";");
                 }
                 writer.println();
             }
